5ac0e91ec71071e59835554909910d49fdc429c2
victor <cavalcantevictor2003@gmail.com>
2025-01-24 19:18:59 -0300

    fun

---
+++ app/src/main/java/com/example/msgapp/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.msgapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val GoldLight = Color(0xFFFFD700)
+val GoldDark = Color(0xFFB8860B)
+val Black = Color(0xFF000000)
+val DarkGray = Color(0xFF1C1C1C)
+val LightGray = Color(0xFF2F2F2F)
